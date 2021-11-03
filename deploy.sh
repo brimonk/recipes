a31a4dd2eccc4ea5c6b008bc4365bdb32eb598ab
Brian Chrzanowski <chrzanowskitest@gmail.com>
2021-11-03 00:50:32 -0400

    added a deploy script which (probably) works?

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Brian Chrzanowski
+# 2021-03-18 17:55:43
+#
+# Deploy to recipes.chrzanowski.me
+#
+# This assumes that `make watch` is running on the server. It just ssh's in, and gets updates from
+# github (or the origin).
+
+REMOTE="chrzanowski.me"
+DIR="/var/www/recipes.chrzanowski.me"
+
+ssh ${REMOTE} "cd ${DIR} ; git fetch"
+ssh ${REMOTE} "cd ${DIR} ; git pull origin master"
+
